19abea95a35ac57abecf4b9cc8e5a7de6b745812
Rohit Jain <rohitjain@Rohits-MacBook-Pro.local>
2021-05-03 22:14:41 +0530

    Resolving Jenkins error

--- service.sh
+++ service.sh
@@ -2,25 +2,25 @@
 #!/bin/sh
 
 source env/bin/activate
+python3 manage.py runserver 0.0.0.0:8000
+# python3 manage.py makemigrations
 
-python3 manage.py makemigrations
+# python3 manage.py migrate
 
-python3 manage.py migrate
+# python3 manage.py collectstatic --noinput
 
-python3 manage.py collectstatic --noinput
+# sudo cp -rf gunicorn.service /etc/systemd/system/
 
-sudo cp -rf gunicorn.service /etc/systemd/system/
+# sudo systemctl daemon-reload
 
-sudo systemctl daemon-reload
+# sudo systemctl start gunicorn
 
-sudo systemctl start gunicorn
+# echo "Gunicorn has started."
 
-echo "Gunicorn has started."
+# sudo systemctl enable gunicorn
 
-sudo systemctl enable gunicorn
+# echo "Gunicorn has been enabled."
 
-echo "Gunicorn has been enabled."
+# sudo systemctl status gunicorn
 
-sudo systemctl status gunicorn
-
-sudo systemctl restart gunicorn
+# sudo systemctl restart gunicorn
