784c5d336f2a009f56446443cdb6fecbd9dae378
Rohit Jain <rohitjain@Rohits-MacBook-Pro.local>
2021-05-03 22:21:07 +0530

    Updated logs and images

--- service.sh
+++ service.sh
@@ -3,24 +3,3 @@
 
 source env/bin/activate
 python3 manage.py runserver 0.0.0.0:8000
-# python3 manage.py makemigrations
-
-# python3 manage.py migrate
-
-# python3 manage.py collectstatic --noinput
-
-# sudo cp -rf gunicorn.service /etc/systemd/system/
-
-# sudo systemctl daemon-reload
-
-# sudo systemctl start gunicorn
-
-# echo "Gunicorn has started."
-
-# sudo systemctl enable gunicorn
-
-# echo "Gunicorn has been enabled."
-
-# sudo systemctl status gunicorn
-
-# sudo systemctl restart gunicorn
